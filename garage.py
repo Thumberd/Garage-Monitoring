2642447c4fb7efafc80001f49e83d8af3d28efc7
Jeremy Albrecht <ajeremyalbrecht@gmail.com>
2016-05-22 18:54:25 +0000

    Multi SMS support

--- garage.py
+++ garage.py
@@ -1,16 +1,42 @@
 import requests
 from chipGPIO import *
 import time
+import json
+import PiCom
 
-PEU = 2
-X = 0
+Fjson = open("data.json", "r")
+data = Fjson.read()
+Fjson.close()
 
-pinMode(PEU, "INPUT")
+def saveData(jsonT):
+    global jsonData
+    Fjson = open("data.json", "w")
+    Fjson.write(jsonT)
+    Fjson.close()
+    jsonData = json.loads(jsonT)
+
+jsonData = json.loads(data)
+
+PEU = jsonData['garage'][0]['gpio']
+X = jsonData['garage'][1]['gpio']
+
+def updateFree():
+    if(PiCom.testConnection()):
+        users = PiCom.getApifree()
+        jsonData['apiFree'] = users
+        saveData(json.dumps(jsonData))
+
+def sendSMS(msg):
+    updateFree()
+    for user in jsonData['apiFree']:
+        r = requests.get("https://smsapi.free-mobile.fr/sendmsg?user=" + str(user['user']) + "&pass=" + str(user['key']) + "&msg=" + str(msg))
+
+pinMode(PEU, INPUT)
 PEUtimeUp = 0
 PEUisUp = False
 PEUnotified = False
 
-pinMode(X, "INPUT")
+pinMode(X, INPUT)
 XtimeUp = 0
 XisUp = False
 Xnotified = False
@@ -25,8 +51,8 @@ while True:
                         PEUtimeUp = time.time()
                         PEUisUp = True
                 print(time.time() - PEUtimeUp)
-                if time.time() - PEUtimeUp >= 600 and PEUnotified == False:
-                        r = requests.get("https://smsapi.free-mobile.fr/sendmsg?user=10908880&pass=9o83gNpCCAMjjs&msg=Garage%20208%20ouvert%20depuis%20plus%20de%2010min.%20Normal%20?")
+                if time.time() - PEUtimeUp >= 300 and PEUnotified == False:
+                        sendSMS("Garage 208 ouvert depuis plus de 5min. Normal ?")
                         PEUnotified = True
         elif PEUval == 0:
                 PEUnotified = False
@@ -39,8 +65,8 @@ while True:
                         XtimeUp = time.time()
                         XisUp = True
                 print(time.time() - XtimeUp)
-                if time.time() - XtimeUp >= 600 and Xnotified == False:
-                        r = requests.get("https://smsapi.free-mobile.fr/sendmsg?user=10908880&pass=9o83gNpCCAMjjs&msg=Garage%20Xsara%20ouvert%20depuis%20plus%20de%2010min.%20Normal%20?")
+                if time.time() - XtimeUp >= 300 and Xnotified == False:
+                        sendSMS("Garage Xsara ouvert depuis plus de 5min. Normal ?")
                         Xnotified = True
         elif Xval == 0:
                 Xnotified = False
